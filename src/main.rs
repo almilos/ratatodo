b0da0f6e6e70118d8aea69967ed610308bc7294e
Aleksandr Miloserdov <miloserdov.a@selectel.ru>
2026-03-02 17:47:21 +0300

    3: Mod and event

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,62 @@
 use ratatui::{
+    DefaultTerminal,
+    Frame,
+    crossterm::event::{self}, // https://docs.rs/crossterm/latest/crossterm/event/index.html
     layout::{
-        Constraint::{Fill, Length, Percentage}, // https://ratatui.rs/examples/layout/constraints/
-        Layout,                                 // https://ratatui.rs/concepts/layout/
+        Constraint::{Fill, Length, Percentage},
+        Layout,
     },
     style::{Color, Style},
     widgets::{Block, List, ListDirection, Paragraph},
 };
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    ratatui::run(|terminal| {
-        terminal.draw(|frame| {
-            let block_list = Block::bordered().title("List"); // блок со списком задач
+// структура, отвечающая за работу нашего tui
+struct Tui {}
 
-            let block_content = Block::bordered() // блок с описанием задачи
-                .title("Content")
-                .border_style(Style::default().fg(Color::Cyan));
+impl Tui {
+    fn new() -> Self {
+        Self {}
+    }
 
-            let block_status = Block::bordered().border_style(Style::default().fg(Color::Cyan)); // блок со статус-баром
+    fn run(mut self, terminal: &mut DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
+        terminal.draw(|frame| self.render(frame))?;
 
-            let items = ["item1", "item2", "item3"]; // заглушка для списка
+        // вместо sleep
+        // ожидаем любой ввод и выходим из приложения
+        let event = event::read()?; //https://docs.rs/crossterm/latest/crossterm/event/index.html
 
-            let list = List::new(items) // https://ratatui.rs/examples/widgets/list/
-                .block(block_list)
-                .highlight_style(Style::new().bg(Color::Red))
-                .direction(ListDirection::TopToBottom);
-
-            let content = Paragraph::new("content") // заглушка для описания задачи
-                .left_aligned()
-                .block(block_content);
-
-            let status = Paragraph::new("status").block(block_status); // заглушка для статуса
-
-            let chunks_v = Layout::vertical(&[Fill(1), Length(3)]).split(frame.area()); // разделяем экран вертикально
-            let chunks_h = Layout::horizontal(&[Percentage(20), Percentage(80)]).split(chunks_v[0]); // разделяем экран горизонтально
-
-            frame.render_widget(list, chunks_h[0]);
-            frame.render_widget(content, chunks_h[1]);
-            frame.render_widget(status, chunks_v[1]);
-        })?;
-        std::thread::sleep(std::time::Duration::from_secs(5));
         Ok(())
-    })
+    }
+
+    fn render(&mut self, frame: &mut Frame) {
+        let block_list = Block::bordered().title("List");
+        let block_content = Block::bordered()
+            .title("Content")
+            .border_style(Style::default().fg(Color::Cyan));
+
+        let block_status = Block::bordered().border_style(Style::default().fg(Color::Cyan));
+
+        let items = ["item1", "item2", "item3"];
+
+        let list = List::new(items)
+            .block(block_list)
+            .highlight_style(Style::new().bg(Color::Red))
+            .direction(ListDirection::TopToBottom);
+
+        let content = Paragraph::new("content")
+            .left_aligned()
+            .block(block_content);
+
+        let status = Paragraph::new("status").block(block_status);
+
+        let chunks_v = Layout::vertical(&[Fill(1), Length(3)]).split(frame.area());
+        let chunks_h = Layout::horizontal(&[Percentage(20), Percentage(80)]).split(chunks_v[0]);
+
+        frame.render_widget(list, chunks_h[0]);
+        frame.render_widget(content, chunks_h[1]);
+        frame.render_widget(status, chunks_v[1]);
+    }
+}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    ratatui::run(|terminal| Tui::new().run(terminal))
 }
