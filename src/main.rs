d5e9fd689ae3e765935ec696d94092b44a8a71d1
Aleksandr Miloserdov <miloserdov.a@selectel.ru>
2026-03-02 17:51:20 +0300

    4: Main loop and handling exit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use ratatui::{
-    DefaultTerminal,
-    Frame,
-    crossterm::event::{self}, // https://docs.rs/crossterm/latest/crossterm/event/index.html
+    DefaultTerminal, Frame,
+    crossterm::event::{self, Event, KeyCode, KeyEvent},
     layout::{
         Constraint::{Fill, Length, Percentage},
         Layout,
@@ -10,20 +9,41 @@ use ratatui::{
     widgets::{Block, List, ListDirection, Paragraph},
 };
 
-// структура, отвечающая за работу нашего tui
-struct Tui {}
+// состояние приложения, пока обозначаем нужно ли выходить или нет
+enum TuiState {
+    Idle,
+    ShouldExit,
+}
+
+struct Tui {
+    state: TuiState,
+}
 
 impl Tui {
     fn new() -> Self {
-        Self {}
+        Self {
+            state: TuiState::Idle,
+        }
+    }
+
+    // https://docs.rs/crossterm/latest/crossterm/event/struct.KeyEvent.html
+    fn handle_key(&mut self, key: KeyEvent) {
+        match key.code {
+            KeyCode::Esc => self.state = TuiState::ShouldExit,
+            _ => {}
+        }
     }
 
     fn run(mut self, terminal: &mut DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
-        terminal.draw(|frame| self.render(frame))?;
-
-        // вместо sleep
-        // ожидаем любой ввод и выходим из приложения
-        let event = event::read()?; //https://docs.rs/crossterm/latest/crossterm/event/index.html
+        // main loop
+        while !matches!(self.state, TuiState::ShouldExit) {
+            terminal.draw(|frame| self.render(frame))?;
+            let event = event::read()?;
+            match event {
+                Event::Key(key) => self.handle_key(key),
+                _ => {}
+            }
+        }
 
         Ok(())
     }
@@ -47,7 +67,7 @@ impl Tui {
             .left_aligned()
             .block(block_content);
 
-        let status = Paragraph::new("status").block(block_status);
+        let status = Paragraph::new("[Esc] to exit").block(block_status);
 
         let chunks_v = Layout::vertical(&[Fill(1), Length(3)]).split(frame.area());
         let chunks_h = Layout::horizontal(&[Percentage(20), Percentage(80)]).split(chunks_v[0]);
